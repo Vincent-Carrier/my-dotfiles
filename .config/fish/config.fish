f181792339a3ad3c485d614a29f9888e181563dc
Vincent-Carrier <vincentcarrier15@gmail.com>
2020-04-19 21:42:50 -0400

    Make fish config host-aware

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,19 +4,20 @@ if not functions -q fisher
     fish -c fisher
 end
 
-# theme
-
 fish_default_key_bindings 
 
-bind \co "ranger; commandline -f repaint"
-bind \cq "clear;fuzzykak; commandline -f repaint"
-bind \ep "theme"
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+if test (hostname) = 'Vincents-MacBook-Pro.local'
+  bind \cq "clear;fuzzykak; commandline -f repaint"
+  bind \ep "theme"
+  bind \co "ranger; commandline -f repaint"
 
-source /usr/local/Cellar/asdf/0.7.6/asdf.fish
+  test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
-eval (direnv hook fish)
+  source /usr/local/Cellar/asdf/0.7.6/asdf.fish
+
+  eval (direnv hook fish)
 
 
-set -x PATH /Users/vincent/.config/nvm/13.10.1/bin /Users/vincent/.local/bin /Users/vincent/.cargo/bin /usr/local/bin /usr/local/bin /Users/vincent/go/bin /usr/bin /bin /sbin /usr/sbin /Library/Frameworks/Python.framework/Versions/3.6/bin
+  set -x PATH /Users/vincent/.config/nvm/13.10.1/bin /Users/vincent/.local/bin /Users/vincent/.cargo/bin /usr/local/bin /usr/local/bin /Users/vincent/go/bin /usr/bin /bin /sbin /usr/sbin /Library/Frameworks/Python.framework/Versions/3.6/bin
+end
